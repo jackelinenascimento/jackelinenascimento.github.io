f9f440b81725d0d3ab2e1f3de55c8130cae39b9a
Jackeline do Nascimento <52252747+jackelinenascimento@users.noreply.github.com>
2020-04-16 00:34:45 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const main = {
+  name: 'Jackeline do Nascimento',
+  mail: 'jackeline_nascimento@hotmail.com',
+  img: './photo-profile.jpg',
+  role: ['Front-End Developer'],
+  
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/jackelinenascimento'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/jackelinenascimento/'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'jackeline_nascimento@hotmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
